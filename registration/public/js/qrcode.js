be174c7486702ecc089760ec50ae5c4193fd0fc7
mindfire <mindfire@mindfire-Vostro-1015>
2016-03-16 10:55:22 +0530

    remove unwanted folder

--- registration/public/js/qrcode.js
+++
@@ -1,8 +0,0 @@
-function printDiv(divName) 
-{
-	 var printContents = document.getElementById(divName).innerHTML;
-	 w=window.open();
-	 w.document.write(printContents);
-	 w.print();
-	 w.close();
-}
