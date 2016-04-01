4ccc6b40a57e99eb355e0229bea5ae83fc5274f5
mindfire <mindfire@mindfire-Vostro-1015>
2016-04-01 19:01:04 +0530

    modify view page

--- website/resources/views/search.blade.php
+++ website/resources/views/search.blade.php
@@ -2,27 +2,24 @@
 
 @section('css')
 <link href="http://netdna.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.css" rel="stylesheet">
-<link href="{{ asset_timed('/css/star-rating.css') }}" media="all" rel="stylesheet" type="text/css" />
 <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
 <link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.14/themes/ui-darkness/jquery-ui.css" />
-<link href="{{ asset_timed('/css/theme-krajee-svg.css') }}" media="all" rel="stylesheet" type="text/css" />
 @endsection
 
 @section('script')
 <script src="//code.jquery.com/jquery-1.10.2.js"></script>
 <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
 <script src="{{ asset_timed('/js/hotel-search.js') }}"></script>
-<script src="{{ asset_timed('/js/filter.js') }}"></script>
 <script src="{{ asset_timed('/js/map-search.js') }}"></script>
 <script src="http://maps.googleapis.com/maps/api/js?key=AIzaSyDDG_qAbS4Ni9qSUtFrdq35VnKLc6GWZN8" type="text/javascript"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.14/jquery-ui.min.js"  type="text/javascript"></script>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.js"></script>
-<script src="{{ asset_timed('/js/star-rating.js') }}" type="text/javascript"></script>
-<script src="{{ asset_timed('/js/star-rating_locale_LANG.js') }}"></script>
+<script src="{{ asset_timed('/js/filter.js') }}"></script>
 @endsection
 
 @section('content')
 
+<!-- main content-->
 <div class="jumbotron">
     <div class="row">
         <div class="col-md-offset-3">
@@ -83,28 +80,91 @@
     </div>
 </div>
 
+<!--Displaying results after API call-->
 @if ( isset($data))
+<script>
+    $(document).ready(function () {
+    var data = '<?php if(isset($data)){ echo addslashes(json_encode($data)); }?>';
+        if (data !== undefined ) {
+            json = $.parseJSON(data);
+            filter(json);
+        }
+    });
+</script>
 <div class="row">
 <div class="col-md-3 col-md-offset-1 filters">
     <div class="panel panel-default">
+
+    <!--Top Filters Options-->
         <div class="panel-heading">Top Filters</div>
             <div class="panel-body">
                 <hr/>
-                <div class="ft-ratings"> Ratings </div>
-                <div>
-                <span id="smile">
-                <img class="emo-sm" hspace="15" src="/img/smiley-face.jpg"></span>
-                <span id="granny">
-                <img class="emo-gr" hspace="15"
-                src="/img/Granny-Enchanted-Neutral-Face.png"></span>
-                <span id="sad">
-                <img class="emo-sad" hspace="15"src="/img/sad.png"></span>
-                </div>
+                    <div class="ft-ratings">Star Ratings: </div>
+                    <div><button id="star1" value="1">
+                    <img class="ft-star" hspace="15" 
+                    src="/img/favorites-star-2.png"></button></div>
+                    <div><button id="star2" value="2">
+                    <img class="ft-star" hspace="15" 
+                    src="/img/favorites-star-2.png">
+                    <img class="ft-star" hspace="15" 
+                    src="/img/favorites-star-2.png"></button></div>
+                    <div><button id="star3" value="3">
+                    <img class="ft-star" hspace="15" 
+                    src="/img/favorites-star-2.png">
+                    <img class="ft-star" hspace="15" 
+                    src="/img/favorites-star-2.png">
+                    <img class="ft-star" hspace="15" 
+                    src="/img/favorites-star-2.png"></button></div>
+                    <div><button id="star4" value="4">
+                    <img class="ft-star" hspace="15" 
+                    src="/img/favorites-star-2.png">
+                    <img class="ft-star" hspace="15" 
+                    src="/img/favorites-star-2.png">
+                    <img class="ft-star" hspace="15" 
+                    src="/img/favorites-star-2.png">
+                    <img class="ft-star" hspace="15" 
+                    src="/img/favorites-star-2.png"></button></div>
+                    <div><button id="star5" value="5">
+                    <img class="ft-star" hspace="15" 
+                    src="/img/favorites-star-2.png">
+                    <img class="ft-star" hspace="15" 
+                    src="/img/favorites-star-2.png">
+                    <img class="ft-star" hspace="15" 
+                    src="/img/favorites-star-2.png">
+                    <img class="ft-star" hspace="15" 
+                    src="/img/favorites-star-2.png">
+                    <img class="ft-star" hspace="15" 
+                    src="/img/favorites-star-2.png"></button></div>
+                <hr/>
+                <hr/>
+                    <div class="ft-ratings">Guest Ratings: </div>
+                    <button id="sad0" value="0">
+                    <img class="ft-emo-sad" hspace="15" src="/img/sad.png"></button>
+                    <button id="granny2" value="2">
+                    <img class="ft-emo-gr" hspace="15" 
+                    src="/img/Granny-Enchanted-Neutral-Face.png"></button>
+                    <button id="smile4" value="4">
+                    <img class="ft-emo-sm" hspace="15" 
+                     src="/img/smiley-face.jpg"></button>
+                <hr/>
+                <hr/>
+                    <div class="ft-ratings">Search Hotel Names: </div>
+                    <div class="row">
+                    <div class="col-md-8"><input id="hotel-name" type="text" 
+                    class="form-control"/></div>
+                    <div class="col-md-2">
+                    <button id="search-hotel" class="btn btn-info">G0..
+                    </button></div>
+                    </div>
                 <hr/>
         </div>
+        <div><button id="reset" class="btn btn-info">
+        Reset Filters</button></div>
     </div>
 </div>
 <div class="col-md-7 all-details">
+
+<!--to display Map-->
 <div id="dialog">
 <div id="dvMap"></div>
 </div>
@@ -115,6 +175,8 @@
     onClick ="hotelMap({{ $lanlog['lat'] }},{{ $lanlog['lng'] }}, {{ json_encode($data)}});">
     Map</button>
 </span> 
+
+<!--sorting hotels-->
 <div class="col-md-3" id="sort">
 <select class="form-control" name="sort" id="sortname">
     <option value="price">Sort By</option>
@@ -123,10 +185,16 @@
     <option value="guestrating">Sort by Popularity</option>
 </select>
 </div>
+
+<!--hidden loading image during ajax call-->
 <div class="loading"><img class="load" src="/img/loadingimage.gif"></div>
 <div id="parentdiv">
 <br/>
-<div id="error"><h3>Sorry...Result not Found..!</h3></div>
+
+<!--To display error message when result not found-->
+<div id="error"><h4>Sorry...No Result Found..!</h4></div>
+
+<!--Display hotels-->
 @for ($i = 0; $i < $total['count']; $i++)
     <div class="col-md-12 hotel-list">
         <div class="row">
@@ -143,10 +211,55 @@
                 <div class="hotel-name">{{ $data[$i]['name'] }}</div>
                 @endif
 
+                @if(isset($data[$i]['ratings']))
                 <div id="stars">
-                <input id="input-id" type="number" class="rating" readonly="false"
-                value="@if(isset($data[$i]['ratings'])){{$data[$i]['ratings']}}@endif">
+                @if ($data[$i]['ratings'] > 0
+                        && $data[$i]['ratings'] <= 1)
+                <span ><img class="rating-stars" 
+                src="/img/favorites-star-2.png"></span>
+                @endif
+                @if ($data[$i]['ratings'] > 1
+                        && $data[$i]['ratings'] <= 2)
+                <span ><img class="rating-stars" 
+                src="/img/favorites-star-2.png"></span>
+                <span ><img class="rating-stars" 
+                src="/img/favorites-star-2.png"></span>
+                @endif
+                @if ($data[$i]['ratings'] > 2
+                        && $data[$i]['ratings'] <= 3)
+                <span ><img class="rating-stars" 
+                src="/img/favorites-star-2.png"></span>
+                <span ><img class="rating-stars" 
+                src="/img/favorites-star-2.png"></span>
+                <span ><img class="rating-stars" 
+                src="/img/favorites-star-2.png"></span>
+                @endif
+                @if ($data[$i]['ratings'] > 3
+                        && $data[$i]['ratings'] <= 4)
+                <span ><img class="rating-stars" 
+                src="/img/favorites-star-2.png"></span>
+                <span ><img class="rating-stars" 
+                src="/img/favorites-star-2.png"></span>
+                <span ><img class="rating-stars" 
+                src="/img/favorites-star-2.png"></span>
+                <span ><img class="rating-stars" 
+                src="/img/favorites-star-2.png"></span>
+                @endif
+                @if ($data[$i]['ratings'] > 4
+                        && $data[$i]['ratings'] <= 5)
+                <span ><img class="rating-stars" 
+                src="/img/favorites-star-2.png"></span>
+                <span ><img class="rating-stars" 
+                src="/img/favorites-star-2.png"></span>
+                <span ><img class="rating-stars" 
+                src="/img/favorites-star-2.png"></span>
+                <span ><img class="rating-stars" 
+                src="/img/favorites-star-2.png"></span>
+                <span ><img class="rating-stars" 
+                src="/img/favorites-star-2.png"></span>
+                @endif
                 </div>
+                @endif
                 
                 @if(isset($data[$i]['address'])) 
                 <div class="hotel-address">{{ $data[$i]['address'] }}</div>
@@ -159,7 +272,8 @@
                         @endif
                         @if ($data[$i]['guestratings'] >= 2
                         && $data[$i]['guestratings'] < 4)
-                        <img class="emo-gr" src="/img/Granny-Enchanted-Neutral-Face.png">
+                        <img class="emo-gr" 
+                        src="/img/Granny-Enchanted-Neutral-Face.png">
                         @endif
                         @if ($data[$i]['guestratings'] < 2)
                         <img class="emo-sad" src="/img/sad.png">
@@ -175,14 +289,15 @@
             <div class="col-md-3 price-details">
                 <div><center>Expedia</center></div>
                 @if(isset($data[$i]['price']))
-                <div class="price"><label>{{'$'}} {{ $data[$i]['price']}} </label></div>
+                <div class="price"><label>{{'$'}} {{ $data[$i]['price']}} 
+                </label></div>
                 @endif
+                @if(isset($data[$i]['detailsurl']))
                 <div class="deal-fit">
-                <input type="hidden" id="url" 
-                value="@if(isset($data[$i]['detailsurl'])){{$data[$i]['detailsurl']}}@endif">
-                <button class="btn btn-success deal" onclick="myFunction();">View Deal
+                <button class="btn btn-success deal" onClick="myFunction('{{$data[$i]['detailsurl']}}');">View Deal
                 <span class="glyphicon glyphicon-chevron-right"></span></button>
                 </div>
+                @endif
             </div>
         </div>     
     </div>
@@ -191,4 +306,5 @@
 </div>
 </div>
 @endif
+
 @endsection
